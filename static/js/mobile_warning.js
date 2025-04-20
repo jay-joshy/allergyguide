62cd3070b3e1b50944623c4e1cf2ae2a67d03dff
jay-joshy <99108373+jay-joshy@users.noreply.github.com>
2025-04-20 15:00:48 -0400

    small wording fix

--- static/js/mobile_warning.js
+++ static/js/mobile_warning.js
@@ -1,34 +1,34 @@
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 if (isMobile) {
-  document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("DOMContentLoaded", function() {
     const container = document.querySelector('.mobile_warning');
-    
+
     if (container) {
-        fetch('/icon/t_danger.svg') // Update the path as needed
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.text();
-            })
-            .then(svgData => {
-                container.innerHTML = `
+      fetch('/icon/t_danger.svg') // Update the path as needed
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.text();
+        })
+        .then(svgData => {
+          container.innerHTML = `
                 <div class="admonition danger">
                   <div class="admonition-icon">
                     ${svgData}
                   </div>
-                  <strong class="admonition-title">THIS DOES NOT WORK ON MOBILE</strong>
+                  <strong class="admonition-title">THIS SECTION IS NOT INTENDED FOR MOBILE</strong>
                   <div class="admonition-content">
-                    The following content is intended for desktop. Expect bugs if you proceed.
+                    The following content is intended for desktop. Expect bugs/missing content in this section.
                   </div>
                 </div>
                 `;
-            })
-            .catch(error => {
-                console.error('Error fetching SVG:', error);
-            });
+        })
+        .catch(error => {
+          console.error('Error fetching SVG:', error);
+        });
     }
-});
+  });
 
 
 }
