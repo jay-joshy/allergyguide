55bc95c2e0d660dcecc0287c8a0d738e0298b599
jay-joshy <99108373+jay-joshy@users.noreply.github.com>
2025-07-25 09:58:45 -0700

    fix cors issue

--- netlify/functions/test_func.js
+++ netlify/functions/test_func.js
@@ -2,9 +2,15 @@ export default async (req, context) => {
   const requestKey = req.headers.get("X-API-Key");
   const apiKey = process.env.SHUFFLE_KEY;
 
+  const headers = {
+    "Access-Control-Allow-Origin": "*", // or a specific domain if you prefer
+    "Access-Control-Allow-Headers": "X-API-Key"
+  };
+
   if (requestKey === apiKey) {
-    return new Response("Welcome!");
+    return new Response("Welcome!", { status: 200, headers });
   }
 
-  return new Response("Sorry, no access for you.", { status: 401 });
+  return new Response("Sorry, no access for you.", { status: 401, headers });
 };
+
