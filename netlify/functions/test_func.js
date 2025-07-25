016b5d052f3662bd91644c312c1ab0d00e1707a7
jay-joshy <99108373+jay-joshy@users.noreply.github.com>
2025-07-25 09:46:30 -0700

    test simple netlify function

---
+++ netlify/functions/test_func.js
@@ -0,0 +1,10 @@
+export default async (req, context) => {
+  const requestKey = req.headers.get("X-API-Key");
+  const apiKey = Netlify.env.get("SHUFFLE_KEY");
+
+  if (requestKey === apiKey) {
+    return new Response("Welcome!");
+  }
+
+  return new Response("Sorry, no access for you.", { status: 401 });
+};
