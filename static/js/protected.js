a59d79256d830b694702f05342013dbf325a837d
jay-joshy <99108373+jay-joshy@users.noreply.github.com>
2025-07-26 13:23:44 -0700

    update back to orig, the decode cleanuip didn't work

--- static/js/protected.js
+++ static/js/protected.js
@@ -1,3 +1,4 @@
+// protected.js
 // Enhanced protected content loader with DEBUG logging
 class ProtectedContentLoader {
   constructor() {
@@ -277,9 +278,7 @@ class ProtectedContentLoader {
     // Fetch all images in parallel
     const imagePromises = images.map(async (img) => {
       console.log(`not cleaned img fullpath: ${img.fullPath}`)
-      let clean_fullPath = img.fullPath.replace(/&#x2F;/g, "/")
-      console.log(`cleaned img fullpath: ${clean_fullPath}`)
-      const dataUrl = await this.fetchImage(clean_fullPath, token);
+      const dataUrl = await this.fetchImage(img.fullPath, token);
 
       console.log(`dataURL: ${dataUrl}`)
       return { ...img, dataUrl };
