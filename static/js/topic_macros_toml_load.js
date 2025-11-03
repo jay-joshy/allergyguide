a2a9c3777bf58fe80a9871f933e83693f4b949e7
jay-joshy <99108373+jay-joshy@users.noreply.github.com>
2025-11-03 09:34:43 -0800

    inject date into copy button for templates
    will break if wording for today's date in template is chnaged...

--- static/js/topic_macros_toml_load.js
+++ static/js/topic_macros_toml_load.js
@@ -1,23 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   document.querySelectorAll(".copy-button").forEach(button => {
-    button.addEventListener("click", function () {
+    button.addEventListener("click", function() {
       copyToClipboard(this);
     });
   });
 });
 
 function copyToClipboard(button) {
-    // THIS IS A COMMENT THAT SHOULD SHOW UP.
-    const codeBlock = button.nextElementSibling; // Get the sibling `.txt` div
-    const text = codeBlock.textContent || codeBlock.innerText;
+  // THIS IS A COMMENT THAT SHOULD SHOW UP.
+  const codeBlock = button.nextElementSibling; // Get the sibling `.txt` div
+  const text = codeBlock.textContent || codeBlock.innerText;
 
-    navigator.clipboard.writeText(text).then(() => {
-        button.textContent = "Copied!"; // Temporarily change button text
-        setTimeout(() => button.textContent = "Copy", 2000); // Revert after 2 seconds
-    }).catch(err => {
-        console.error("Failed to copy text: ", err);
-        button.textContent = "Error";
-        setTimeout(() => button.textContent = "Copy", 2000);
-    });
+  // Inject date into text if {{date}}
+  if (text.substring("We assessed this patient today on ***")) {
+    const today = new Date();
+    const options = { day: '2-digit', month: 'short', year: 'numeric' };
+    const formattedDate = today
+      .toLocaleDateString('en-GB', options)
+      .replace(/ /g, '-');
+    text.replace("We assessed this patient today on ***", "We assessed this patient today on " + formattedDate)
+  }
+
+  navigator.clipboard.writeText(text).then(() => {
+    button.textContent = "Copied!"; // Temporarily change button text
+    setTimeout(() => button.textContent = "Copy", 2000); // Revert after 2 seconds
+  }).catch(err => {
+    console.error("Failed to copy text: ", err);
+    button.textContent = "Error";
+    setTimeout(() => button.textContent = "Copy", 2000);
+  });
 }
 
