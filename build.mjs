7382900c9623f37a8ed31379e1351df3efc3788b
jay-joshy <99108373+jay-joshy@users.noreply.github.com>
2025-12-03 18:25:37 -0800

    remake build.mjs and move oit calc to its own folder
    TODO update a readme to make this setup more defined

--- build.mjs
+++ build.mjs
@@ -4,8 +4,28 @@ import { execSync } from 'child_process';
 const commit_hash = execSync('git rev-parse --short HEAD').toString().trim();
 console.log(`Building with commit hash: ${commit_hash}`);
 
-// esbuild command with the define flag
-const command = `esbuild static/ts/**/*.ts --bundle --outdir=static/js/ --format=esm --splitting --target=es2022 --minify --chunk-names=chunks/[name]-[hash] --define:import.meta.vitest=undefined --define:__COMMIT_HASH__='${JSON.stringify(commit_hash)}'`
+// DEFINES
+const defines = [
+  `--define:import.meta.vitest=undefined`,
+  `--define:__COMMIT_HASH__='${JSON.stringify(commit_hash)}'`
+].join(' ');
+
+// ENTRY POINTS
+// Format: "OutputFileName=InputSourceFile"
+// Creates static/js/{OutputFileName}.js
+// example for single TS file: `tabs=static/ts/tabs.ts`
+// example for TS folder proj: `oit_calculator=static/ts/oit_calculator/main.ts`
+const entryPoints = [
+  "oit_calculator=static/ts/oit_calculator/main.ts",
+].join(' ');
+
+// esbuild command 
+const command = `esbuild ${entryPoints} --bundle --outdir=static/js/ --format=esm --splitting --target=es2022 --minify --chunk-names=chunks/[name]-[hash] ${defines}`;
 
 // Run
-execSync(command, { stdio: 'inherit' });
+try {
+  execSync(command, { stdio: 'inherit' });
+} catch (error) {
+	console.log("build.mjs failed:", error)
+  process.exit(1);
+}
