34b57a0dd3342b2b1e5cca46f47ae8d1611a9615
jay-joshy <99108373+jay-joshy@users.noreply.github.com>
2025-12-13 14:27:32 -0800

    remove fetch

--- netlify/functions/protected-image.js
+++ netlify/functions/protected-image.js
@@ -1,6 +1,4 @@
 // netlify/functions/protected-image.js
-const fetch = require('node-fetch'); // Only needed in local dev
-
 exports.handler = async (event) => {
   try {
     // Parse environment variables
